1541d3ed2579140cc143f82f67d85d9d3b682045
Matt Felock <matt.felock@vuhl-t6jk20n.vuhl.root.mrc.local>
2022-01-28 12:04:38 -0600

    added try & except to determine if input is valid

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -3,19 +3,23 @@ def calculateGrade():
     print("Calculating Grade")
     # This first line is provided for you
 
-    score = float(input("Enter score: "))
-    if score >= 0.9 and score <= 1 and score >= 0:
-        print("A")
-    elif score >= 0.8 and score <= 1 and score >= 0:
-        print("B")
-    elif score >= 0.7 and score <= 1 and score >= 0:
-        print("C")
-    elif score >= 0.6 and score <= 1 and score >= 0:
-        print("D")
-    elif score < 0.6 and score <= 1 and score >= 0:
-        print("F")
-    elif score > 1.0 or score < 0.0:
-        print("score out of range")
+    score = input("Enter score: ")
+    try:
+        score = float(score)
+        if score >= 0.9 and score <= 1 and score >= 0:
+            print("A")
+        elif score >= 0.8 and score <= 1 and score >= 0:
+            print("B")
+        elif score >= 0.7 and score <= 1 and score >= 0:
+            print("C")
+        elif score >= 0.6 and score <= 1 and score >= 0:
+            print("D")
+        elif score < 0.6 and score <= 1 and score >= 0:
+            print("F")
+        elif score > 1.0 or score < 0.0:
+            print("score out of range")
+    except:
+        print("invalid score")
 
     # end assignment
 
