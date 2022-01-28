dc73db11991e335d5c5c2f74d5edd72974b50cdc
Matt Felock <matt.felock@vuhl-t6jk20n.vuhl.root.mrc.local>
2022-01-28 11:41:56 -0600

    added more restrictions to display score out of range

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -4,17 +4,17 @@ def calculateGrade():
     # This first line is provided for you
 
     score = float(input("Enter score: "))
-    if score >= 0.9:
+    if score >= 0.9 and score <= 1 and score >= 0:
         print("A")
-    elif score >= 0.8:100
+    elif score >= 0.8 and score <= 1 and score >= 0:
         print("B")
-    elif score >= 0.7:
+    elif score >= 0.7 and score <= 1 and score >= 0:
         print("C")
-    elif score >= 0.6:
+    elif score >= 0.6 and score <= 1 and score >= 0:
         print("D")
-    elif score < 0.6:
+    elif score < 0.6 and score <= 1 and score >= 0:
         print("F")
-    elif score > 1.0:
+    elif score > 1.0 or score < 0.0:
         print("score out of range")
 
     # end assignment
